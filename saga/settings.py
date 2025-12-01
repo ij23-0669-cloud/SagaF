688c55cc39bd330a3b1704bb41aa1c5d8eb85e3d
ij23-0669-cloud <ij23-0669@unphu.edu.do>
2025-11-30 20:08:59 -0400

    puse el icono
    eso que dice ahí

--- saga/settings.py
+++ saga/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+9x^70#@qms0+wxo6fll!((615t!=fxiaw@udlww4x@b8)$i6r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['YisusCry.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
